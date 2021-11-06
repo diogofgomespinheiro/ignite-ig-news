88cd0fe89ca52b220629a58c4c2897a8b7f3138f
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-11-06 02:23:36 +0000

    ♻️  chore: add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  jsxBracketSameLine: false,
+  printWidth: 100,
+  proseWrap: "preserve",
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "none",
+  useTabs: false,
+};
