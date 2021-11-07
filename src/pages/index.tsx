1eef2f02c021fc95504a251172e35d6ecf5072af
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-11-07 17:06:05 +0000

    ✨ feat: add auth with github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,14 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import { GetStaticProps } from 'next';
 
-import SubscribeButton from '../components/subscribe-button/subscribe-button';
+import HomeScreen, { HomeScreenProps } from '../screens/home';
 import { stripe } from '../services/stripe';
 import { formatCurrency } from '../utils';
 
-import styles from '../styles/pages/home.module.scss';
-
-type HomeProps = {
-  product: {
-    priceId: string;
-    amount: number;
-  };
-};
-
-export default function Home({ product }: HomeProps) {
-  return (
-    <>
-      <Head>
-        <title>Home | ig.news</title>
-      </Head>
-      <main className={styles.contentContainer}>
-        <section className={styles.hero}>
-          <span>👋 Hey, welcome</span>
-          <h1>
-            News about the <span>React</span> World.
-          </h1>
-          <p>
-            Get access to all the publications <br />
-            <span>for {product.amount} month</span>
-          </p>
-          <SubscribeButton priceId={product.priceId} />
-        </section>
-        <Image src="/assets/avatar.svg" alt="Girl Coding" height={521} width={336} />
-      </main>
-    </>
-  );
+export default function Home(props: HomeScreenProps) {
+  return <HomeScreen {...props} />;
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps<HomeScreenProps> = async () => {
   const price = await stripe.prices.retrieve('price_1JstpyG8IwdLJkAnAdpJZ8M2');
 
   const product = {
---
+++ src/screens/home/home.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+import SubscribeButton from '../../components/subscribe-button/subscribe-button';
+
+import styles from './styles.module.scss';
+
+export type HomeScreenProps = {
+  product: {
+    priceId: string;
+    amount: string;
+  };
+};
+
+function HomeScreen({ product }: HomeScreenProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> World.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+        <Image src="/assets/avatar.svg" alt="Girl Coding" height={521} width={336} />
+      </main>
+    </>
+  );
+}
+
+export default HomeScreen;
