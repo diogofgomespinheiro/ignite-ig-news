3c6045c6abc3e4064d82d72f6ba8e358542f77c1
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-11-07 22:38:30 +0000

    ✨ feat: add user creation on db when signin is done

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+
+import { fauna } from '@services/fauna';
 
 export default NextAuth({
   providers: [
@@ -8,5 +12,28 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     })
-  ]
+  ],
+  jwt: {
+    signingKey: process.env.JWT_SIGNING_PRIVATE_KEY
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+          )
+        );
+
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    }
+  }
 });
