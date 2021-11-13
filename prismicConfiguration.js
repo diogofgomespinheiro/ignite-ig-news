12c4eca03b0c03a1f08a6ef56fcbe48f34400bf7
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-11-13 14:25:30 +0000

    ♻️  chore: add prismic dependencies

---
+++ prismicConfiguration.js
@@ -0,0 +1,3 @@
+export const repoName = process.env.PRISMIC_REPO_NAME;
+export const accessToken = process.env.PRISMIC_ACCESS_TOKEN;
+export const apiEndpoint = `https://${repoName}.prismic.io/api/v2`;
